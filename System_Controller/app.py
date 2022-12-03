735f9b84a8138b156ffb4da9348c6210c418718f
PabloRomero <pabloromeroenjamio@gmail.com>
2022-12-02 22:20:01 -0300

    Added SQL creator

--- System_Controller/app.py
+++ System_Controller/app.py
@@ -1,13 +1,55 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from flaskext.mysql import MySQL
+from datetime import datetime
+
 
 app = Flask(__name__)
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'cacback'
+mysql.init_app(app)
+
+
 @app.route('/')
-def index():
+def index(): 
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'MARADO', 'DIAMARIA@ciudad.com.ar', 'fotolademessi.jpg');"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    return render_template('empleados/index.html')
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre  = request.form['txtNombre']
+    _correo  = request.form['txtCorreo']
+    _foto    = request.files['txtFoto']
+
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    if _foto.filename != '':
+        nuevoNombreFoto=tiempo + _foto.filename
+        _foto.save("System_Controller/uploads/"+nuevoNombreFoto)
+
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    datos=(_nombre, _correo , nuevoNombreFoto)
+    
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
     return render_template('empleados/index.html')
 
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
